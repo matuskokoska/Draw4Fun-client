e5275d33cec0f5c20442d6acea1d3e51740fac34
Michal <michal.borovsky@akademiasovy.sk>
2016-04-21 14:34:48 +0200

    redesigned login form

--- Draw4Fun - client/Form1.Designer.cs
+++ Draw4Fun - client/Form1.Designer.cs
@@ -36,11 +36,13 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(227, 380);
+            this.button1.Location = new System.Drawing.Point(390, 387);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(173, 25);
             this.button1.TabIndex = 0;
@@ -50,7 +52,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(227, 349);
+            this.button2.Location = new System.Drawing.Point(390, 356);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(173, 25);
             this.button2.TabIndex = 1;
@@ -60,7 +62,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(190, 179);
+            this.button3.Location = new System.Drawing.Point(353, 186);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(110, 35);
             this.button3.TabIndex = 2;
@@ -69,7 +71,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(190, 220);
+            this.button4.Location = new System.Drawing.Point(353, 227);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(110, 35);
             this.button4.TabIndex = 3;
@@ -79,7 +81,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(306, 179);
+            this.textBox1.Location = new System.Drawing.Point(469, 186);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(167, 35);
@@ -87,7 +89,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(306, 220);
+            this.textBox2.Location = new System.Drawing.Point(469, 227);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(167, 35);
@@ -96,7 +98,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(240, 274);
+            this.button5.Location = new System.Drawing.Point(403, 281);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(88, 25);
             this.button5.TabIndex = 6;
@@ -105,18 +107,30 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(349, 274);
+            this.button6.Location = new System.Drawing.Point(512, 281);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(88, 25);
             this.button6.TabIndex = 7;
             this.button6.Text = "Sign In";
             this.button6.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Draw4Fun___client.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(70, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(847, 108);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 452);
+            this.BackgroundImage = global::Draw4Fun___client.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(1017, 631);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.textBox2);
@@ -127,6 +141,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Welcome";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,6 +157,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
