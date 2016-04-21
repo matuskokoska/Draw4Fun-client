66a4cf417e9ab176bd1b09c7200135efc42eb8c9
matuskokoska <matus.kokoska@akademiasovy.sk>
2016-04-21 17:44:52 +0200

    server server

--- ServerData/Packet.cs
+++ ServerData/Packet.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServerData;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
 
 namespace ServerData
 {
-    public class Class1
+    public class Packet
     {
+
+        public static string GetIP4Address()
+        {
+            IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach(IPAddress i in ips)
+            {
+                if (i.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return i.ToString();
+                }
+            }
+            return "127.0.0.1";
+        }
+
     }
 }
