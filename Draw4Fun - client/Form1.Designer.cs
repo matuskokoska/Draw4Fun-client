707cef20cc01f0f0d72e7b9d3e6dab69e54a85ab
matuskokoska <matus.kokoska@akademiasovy.sk>
2016-04-21 18:46:31 +0200

    repair attempt

--- Draw4Fun - client/Form1.Designer.cs
+++ Draw4Fun - client/Form1.Designer.cs
@@ -29,33 +29,135 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(179, 319);
+            this.button1.Location = new System.Drawing.Point(390, 387);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(354, 105);
+            this.button1.Size = new System.Drawing.Size(173, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "GUESS";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DRAW";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(353, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Login:";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(353, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Password:";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(469, 186);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 35);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(469, 227);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 35);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(403, 281);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 25);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Register";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(512, 281);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(88, 25);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Sign In";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Draw4Fun___client.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(70, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(847, 108);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 436);
+            this.BackgroundImage = global::Draw4Fun___client.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(1017, 631);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Welcome";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Draw4Fun - client/Form1.cs
+++ Draw4Fun - client/Form1.cs
@@ -23,5 +23,16 @@ namespace Draw4Fun___client
             guess.ShowDialog();
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Draw.Draw draw = new Draw.Draw();
+            draw.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
