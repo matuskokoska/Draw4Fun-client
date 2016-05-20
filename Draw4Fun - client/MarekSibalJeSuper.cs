1972e6434acc07681261cf140a2a8cd8377d6460
branislavdavid <branislav.david@akademiasovy.sk>
2016-05-20 11:38:55 +0200

    added class MarekSibalJeSuper.cs

---
+++ Draw4Fun - client/MarekSibalJeSuper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw4Fun___client
+{
+    class MarekSibalJeSuper
+    {
+        async static void getRequest(string url)
+        {
+            using (HttpClient client=new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+                        Console.WriteLine(mycontent);
+                    }
+                }
+            }
+        }
+
+    }
+}
