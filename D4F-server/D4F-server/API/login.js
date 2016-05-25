ded99534adb42cf32592161c9f44efc14179a439
matuskokoska <matus.kokoska@aademiasovy.sk>
2016-05-25 14:22:09 +0200

    login fix 3.0

--- D4F-server/D4F-server/API/login.js
+++ D4F-server/D4F-server/API/login.js
@@ -4,15 +4,23 @@
 module.exports = {
 	login: function (nickname, password, callback){
         db.query("SELECT id, password FROM users WHERE nickname LIKE '" + nickname + "'", function(result) {
-  							if(result[0].password==password){
-									var success=true;
-								}
-								else{
-									var success=false;
-								}
-								if (typeof callback === "function") {
-				                    callback(success);
-				        }
+						if(result.length!=0){
+							if(result[0].password==password){
+								var success=true;
+							}
+							else{
+								var success=false;
+							}
+							if (typeof callback === "function") {
+													callback(success);
+							}
+						}
+						else{
+							if (typeof callback === "function") {
+													callback(false);
+							}
+						}
+
         });
 	}
 };
