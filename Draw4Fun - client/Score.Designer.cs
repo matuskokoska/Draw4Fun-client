dd47fd925fb9ed823c93affc9f6f20f6bc4e04e2
Michal Borovsky <michal.borovsky@akademiasovy.sk>
2016-06-06 08:58:31 +0200

    scoreboard prepared, guess req modified

--- Draw4Fun - client/Score.Designer.cs
+++ Draw4Fun - client/Score.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(85, 463);
+            this.button1.Location = new System.Drawing.Point(98, 463);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 24);
             this.button1.TabIndex = 1;
@@ -54,12 +55,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 394);
+            this.listBox1.TabIndex = 2;
+            // 
             // Score
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Draw4Fun___client.Properties.Resources.bg;
             this.ClientSize = new System.Drawing.Size(284, 499);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Score";
@@ -73,5 +83,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
