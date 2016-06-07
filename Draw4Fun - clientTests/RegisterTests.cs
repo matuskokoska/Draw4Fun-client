2260da9af81a815875ef438c13364a55221d1a28
mareksibal <marek.sibal@akademiasovy.sk>
2016-06-07 13:06:46 +0200

    Unit tests.

--- Draw4Fun - clientTests/RegisterTests.cs
+++ Draw4Fun - clientTests/RegisterTests.cs
@@ -14,19 +14,40 @@ namespace Draw4Fun___client.Tests
         [TestMethod()]
         public void isUsernameValidTest()
         {
+            RegisterTests test = new RegisterTests();
+            Register reg = new Register();
+            Assert.IsNotNull(reg.isUsernameValid("karol"));
+            Assert.IsTrue(reg.isUsernameValid("slovensko"));
+            Assert.IsFalse(reg.isUsernameValid("\"; drop users"));
+            Assert.IsTrue(reg.isUsernameValid("oke"));
+            Assert.IsFalse(reg.isUsernameValid("kk"));
+            
+
 
         }
 
         [TestMethod()]
         public void isPasswordValidTest()
         {
+            RegisterTests test = new RegisterTests();
+            Register reg = new Register();
+            Assert.IsNotNull(reg.isPasswordValid("12345678"));
+            Assert.IsTrue(reg.isPasswordValid("yamahadotjap"));
+            Assert.IsTrue(reg.isPasswordValid("Kamikadze123SK"));
+            Assert.IsFalse(reg.isPasswordValid("heslo"));
 
         }
 
         [TestMethod()]
         public void isPasswordEqualTest()
         {
-
+            RegisterTests test = new RegisterTests();
+            Register reg = new Register();
+            Assert.AreEqual(reg.isPasswordEqual("hesloheslo", "hesloheslo"), true);
+            Assert.AreEqual(reg.isPasswordEqual("hesloheslo", "hesloheslO"), false);
+            Assert.AreNotEqual(reg.isPasswordEqual("heslo", "heslo5"), true);
+            Assert.AreNotEqual(reg.isPasswordEqual("12345", "12345"), false);
+            Assert.IsNotNull(reg.isPasswordEqual("heslo", "heslo"));
         }
     }
 }
